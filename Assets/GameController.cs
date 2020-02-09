b0bf3d5826c5d297c568313905b62aca1185a3f3
Teo G Nistor <teo.g.nistor@gmail.com>
2020-02-09 13:21:17 +0000

    Minuscule fix

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -3,100 +3,64 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-public class GameController : MonoBehaviour
-{
-    private static GameController instance = null;
+public class GameController : MonoBehaviour {
     public Dictionary<string, int> inventory = new Dictionary<string, int>();
-    public GameObject camera;
-    public Light light;
+    public new GameObject camera;
+    public new Light light;
 
     public GameObject playerCamera;
     public GameObject textBox;
     public Text textText;
 
-    public GameObject inventoryShow;
-    public GameObject inventoryText;
-
     public GameObject playerPic;
     public GameObject priestPic;
 
     public Text objectives;
-  
+
     public bool outside;
-    public int score=0;
+    public int score = 0;
 
     // Game Instance Singleton
-    public static GameController Instance
-    {
-        get
-        {
-            return instance;
+    public static GameController Instance { get; private set; }
+
+    private void Awake () {
+        if (Instance != null && Instance != this) {
+            Destroy(gameObject);
+        } else {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
     }
 
-    private void Awake()
-    {
-        // if the singleton hasn't been initialized yet
-        if (instance != null && instance != this)
-        {
-            Destroy(this.gameObject);
-        }
+    void Update () {
 
-        instance = this;
-        DontDestroyOnLoad(this.gameObject);
-    }
-
-    void Update()
-    {
-   
-        if (outside)
-        {
+        if (outside) {
             camera.SetActive(false);
             playerCamera.SetActive(true);
         }
-        if (Input.GetKey("space"))
-        {
+        if (Input.GetKey("space")) {
             textBox.SetActive(false);
-            showInventory(false);
-        }
-
-        if (Input.GetKeyDown("i"))
-        {
-            showInventory(true);
         }
     }
 
-    public void say(string msg)
-    {
+    public void say (string msg) {
         textBox.SetActive(true);
         priestPic.SetActive(false);
         playerPic.SetActive(true);
         textText.text = msg;
     }
 
-    public void sayPriest(string msg)
-    {
+    public void sayPriest (string msg) {
         textBox.SetActive(true);
         priestPic.SetActive(true);
         playerPic.SetActive(false);
         textText.text = msg;
     }
 
-    public void showInventory(bool isShown)
-    {
-        inventoryShow.SetActive(isShown);
-        Text textInv = inventoryText.GetComponent<Text>();
-        textInv.text = "";
-        foreach (KeyValuePair<string, int> entry in inventory)
-        {
-            textInv.text += entry.Key + " : " + entry.Value;
-        }
 
-    }
-
-    public void updateObj(int stage){
+    public void updateObj (int stage) {
         if (stage == 0) objectives.text = "Objectives\n-Find the key\n-Complete the ritual";
-        if( stage == 1) objectives.text = "Objectives\n-Go to the church\n-Complete the ritual ";
+        if (stage == 1) objectives.text = "Objectives\n-Go to the church\n-Complete the ritual ";
         if (stage == 2) objectives.text = "Objectives\n-Complete the ritual ";
         if (stage == 666) objectives.text = "RITUAL\nCOMPLETED";
     }
